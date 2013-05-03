b2be05fd323fd0626c947241a547c0defe616769
Ian <info@ianfeather.co.uk>
2013-05-03 17:46:56 +0100

    Added aws set up task

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
-# Ask for the administrator password upfront
-sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Use AirDrop over every interface. srsly this should be a default.
+echo "Use AirDrop over every interface. srsly this should be a default."
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Enable javascript console
+echo "Enable javascript console"
 sudo ln /System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc /bin/jsc
 
-# Disable the crash reporter
+echo "Disable the crash reporter"
 #defaults write com.apple.CrashReporter DialogType -string "none"
 
-# Disable auto-correct
+echo "Disable auto-correct"
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# Save screenshots to the desktop
+echo "Save screenshots to the desktop"
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+echo "Finder: allow quitting via ⌘ + Q;"
 defaults write com.apple.finder QuitMenuItem -bool true
 
-# Finder: show hidden files by default
-defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Disable the warning when changing a file extension
+echo "Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+echo "Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app"
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
-# Speed up repeat rate
+echo "Speed up repeat rate"
 defaults write NSGlobalDomain KeyRepeat -int 0
-
 
 for app in "Address Book" "Calendar" "Contacts" "Dashboard" "Dock" "Finder" \
   "Mail" "Safari" "SystemUIServer" "Terminal" "iCal"; do
